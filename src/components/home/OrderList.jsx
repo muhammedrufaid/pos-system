1357dbd2de8ba7c49316daefce3854937288f495
muhammedrufaid <mohdrufaid7@gmail.com>
2026-01-11 02:53:00 +0400

    ready text hide

--- src/components/home/OrderList.jsx
+++ src/components/home/OrderList.jsx
@@ -15,7 +15,7 @@ const OrderList = () => {
                     <h1 className='text-[#f6b100] font-medium text-xs border border-[#f6b100] rounded-lg p-1'>Table No : 3</h1>
                 </div>
                 <div className='flex flex-col items-start gap-2'>
-                    <p className='text-green-600 px-4 text-sm' > <FaCheckDouble className='inline mr-2' /> Ready</p>
+                    {/* <p className='text-green-600 px-4 text-sm' > <FaCheckDouble className='inline mr-2' /> Ready</p> */}
                     <p className='text-[#ababab] text-xs'><FaCircle className='inline mr-2 text-green-600' /> Ready to serve</p>
 
                 </div>
